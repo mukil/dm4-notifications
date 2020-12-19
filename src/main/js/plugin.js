32e02348dd367b27a083ff856c7c970a208a970f
mukil <malte@dmx.berlin>
2020-12-19 22:56:01 +0100

    Determine contextCommand per asyncOp failed (#7)

--- src/main/js/plugin.js
+++ src/main/js/plugin.js
@@ -1,77 +1,86 @@
-export default ({dmx, store, axios: http, Vue}) => ({
+export default function ({dmx, store, axios: http, Vue}) {
 
-  init () {
-    console.log("[DMX Notifications] Initializing")
-    store.dispatch("_loadUnseenNotifications", { username: "me"})
-    // React to login! and logout!
-  },
-
-  storeModule: {
-    name: 'notifications',
-    module: require('./notifications-store').default
-  },
-
-  extraElementUI: true,
-
-  components: [{
-    comp: require('./components/notifications-menu').default,
-    mount: 'toolbar-right'
-  }],
-
-  contextCommands: {
-    topic: topic => {
-      let isLoggedIn = (store.state.accesscontrol.username)
-      if ((isLoggedIn && topic.typeUri === 'dmx.topicmaps.topicmap') || (isLoggedIn && topic.typeUri === 'dmx.notes.note') ||
-         (isLoggedIn && topic.typeUri === 'dmx.workspaces.workspace') || (isLoggedIn && topic.typeUri === 'dmx.events.event')) { 
-        // Fixme: allow subscription of "Tags" again
-        // 1) Check: if alread subscribed
-        /** let isSubscribed = false
-        dmx.rpc.getTopicRelatedTopics(topic.id, {
-          assocTypeUri: "dmx.notification_subscription_edge",
-          othersTopicTypeUri: "dmx.accesscontrol.username"
-        }).then(response => {
-          console.log("[Notifications] is my username subscribed?", response)
-        }) **/
-        // 2) Add "Subscribe" and "Unsubscribe" commands
-        return [{
-          label: 'Subscribe',
-          handler: id => {
-              http.post(`/notifications/subscribe/${id}`).then(function (response) {
-                Vue.prototype.$notify({
-                  title: "Subscribed to " + dmx.typeCache.getTopicType(topic.typeUri).value + " " + topic.value,
-                  type: "success"
-                })
-              }).catch(function (error) {
-                console.log("error", error)
-                Vue.prototype.$notify({
-                  title: "Subscribing to " + dmx.typeCache.getTopicType(topic.typeUri).value  + " failed",
-                  message: error.toString(),
-                  type: "error"
-                })
-              })
-            }
-          },
-          {
-            label: 'Unsubscribe',
-            handler: id => {
-              http.post(`/notifications/unsubscribe/${id}`).then(function (response) {
-                Vue.prototype.$notify({
-                  title: "Unsubscribed from " + dmx.typeCache.getTopicType(topic.typeUri).value + " " + topic.value,
-                  type: "success"
-                })
-              }).catch(function (error) {
-                console.log("error", error)
-                Vue.prototype.$notify({
-                  title: "Unsubscribing from " + dmx.typeCache.getTopicType(topic.typeUri).value + " " + topic.value + " failed",
-                  message: error.toString(),
-                  type: "error"
-                })
-              })
-            }
-          }
-        ]
-      }
+  const subscribeCmd = {
+    label: 'Subscribe',
+    handler: id => {
+      http.post(`/notifications/subscribe/${id}`).then(function (response) {
+        Vue.prototype.$notify({
+          title: "Subscribed to " + dmx.typeCache.getTopicType(topic.typeUri).value + " " + topic.value,
+          type: "success"
+        })
+      }).catch(function (error) {
+        console.log("error", error)
+        Vue.prototype.$notify({
+          title: "Subscribing to " + dmx.typeCache.getTopicType(topic.typeUri).value  + " failed",
+          message: error.toString(),
+          type: "error"
+        })
+      })
     }
   }
 
-})
+  const unsubscribeCmd = {
+    label: 'Unsubscribe',
+    handler: id => {
+      http.post(`/notifications/unsubscribe/${id}`).then(function (response) {
+        Vue.prototype.$notify({
+          title: "Unsubscribed from " + dmx.typeCache.getTopicType(topic.typeUri).value + " " + topic.value,
+          type: "success"
+        })
+      }).catch(function (error) {
+        console.log("error", error)
+        Vue.prototype.$notify({
+          title: "Unsubscribing from " + dmx.typeCache.getTopicType(topic.typeUri).value + " " + topic.value + " failed",
+          message: error.toString(),
+          type: "error"
+        })
+      })
+    }
+  }
+
+  function determineCommands(topic) {
+
+    const isLoggedIn = (store.state.accesscontrol.username)
+    const isSupportedType = (
+        topic.typeUri === 'dmx.topicmaps.topicmap' ||
+        topic.typeUri === 'dmx.notes.note' ||
+        topic.typeUri === 'dmx.workspaces.workspace' || 
+        topic.typeUri === 'dmx.events.event')
+    // 1) topic is unsupported
+    if (!isLoggedIn || !isSupportedType) return []
+    // 2) topic is supported
+    return http.get('/notifications/subscription/' + topic.id).then(result => {
+      return (result.data) ? [unsubscribeCmd] : [subscribeCmd]
+    })
+
+  }
+  
+  return {
+    init () {
+      if (store.state.accesscontrol.username) {
+        store.dispatch("_loadUnseenNotifications", { username: "me"}) 
+      }
+      // React to login! and logout!
+    },
+
+    storeModule: {
+      name: 'notifications',
+      module: require('./notifications-store').default
+    },
+
+    extraElementUI: true,
+
+    components: [{
+      comp: require('./components/notifications-menu').default,
+      mount: 'toolbar-right'
+    }],
+
+    contextCommands: {
+      topic: topic => determineCommands(topic).then(result => {
+        console.log("determinedCommands", result)
+        return result
+      })
+    }
+  }
+
+}
